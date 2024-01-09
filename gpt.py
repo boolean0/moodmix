5bc8cf9e3866f61dfe603226b9b9f3040b8847e8
Matt <matthuang03@gmail.com>
2024-01-08 23:51:35 -0500

    add parsing

--- gpt.py
+++ gpt.py
@@ -8,15 +8,28 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 
 
 def get_recs_from_gpt(user_mood, user_artists): 
-    msg = f'{user_mood} songs from {user_artists} in one list in the form \' artist - song \', no additional text, scramble the order, no quotation marks, at least 10 songs'
+    msg = f'{user_mood} songs from {user_artists} in one list in the form \' artist +-+ song \', no additional text, scramble the order, no quotation marks, at least 10 songs'
     resp = client.chat.completions.create(
-        temperature=0.8,
+        temperature=0.3,
         model='gpt-3.5-turbo', 
         messages = [
             {'role': 'user', 
              'content': msg}
         ])
     resp_content = resp.choices[0].message.content
-    print(resp_content)
+    return parse_gpt_result(resp_content)
 
-get_recs_from_gpt('sad', 'lauv, joji, rini')
+def parse_gpt_result(resp_content):
+    artist_and_songs = resp_content.splitlines()
+    ret = [] #list of [song, artist]
+    for line in artist_and_songs:
+        split = line.split('+-+') #song +-+ 
+        ret.append(split)
+
+    return ret
+
+
+
+
+
+get_recs_from_gpt('sad', 'itzy, blackpink')
